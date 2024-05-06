974d2b8d576b06901ea41e309f1ce5853b69121f
Guilhermefonseca2021 <gsbloogs194@gmail.com>
2024-05-05 21:44:48 -0300

    controlling routes

--- src/main/java/com/todo/controllers/TodoController.java
+++ src/main/java/com/todo/controllers/TodoController.java
@@ -1,35 +1,54 @@
 package com.todo.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.todo.models.Todo;
 import com.todo.services.TodoService;
 
 public class TodoController {
     @Autowired
     private TodoService todoService;
-    @GetMapping
-    public String viewAllTodoItems() {
-
+    @GetMapping("/viewTodoList")
+    public String viewAllTodoItems(Model model, @ModelAttribute("message") String message) {
+        model.addAttribute("list", todoService.getAllTodoItems());
+        model.addAttribute("msg", message);
+        return "ViewTodoList";
     }
-    @PutMapping
-    public String UpdateTodoStatus() {
 
+    @PutMapping("/updatetodostatus/{id}")
+    public String UpdateTodoStatus(@PathVariable Long id, RedirectAttributes redirectAttributes) {
+        if(todoService.updateTodoStatus(id)) {
+            redirectAttributes.addFlashAttribute("message", "update success!");
+            return "redirect:/viewTodoList";
+        }
+        redirectAttributes.addFlashAttribute("message", "Updare failure!");
+        return "redirect:/viewTodoList";
     }   
-    @PostMapping
-    public String addTodo() {
 
+    @GetMapping("/addtodoitem")
+    public String addTodoItem(Model model) {
+        model.addAttribute("todo", new Todo());
+        return "addtodoitem";
     }
-    @PostMapping
-    public String saveTodoItem() {
 
-    }
-    @PostMapping
-    public String editSaveTodoItem() {
+    @PostMapping("/savetodoitem")
+    public String saveTodoItem(Todo todo) {
+        if(todoService.saveOrUpdateTodoItem(todo)) {
 
+        }
+        return "redirect:/addtodoitem";
     }
+    // @PostMapping
+    // public String editSaveTodoItem() {
+
+    // }
 }
 
 
